feb20a7408d234e01d9076d821b9a8de9464a871
no-vac <crysist14@gmail.com>
2019-03-09 15:58:48 -0500

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,17 +8,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="soon">
+            <p>COMING SOON</p>
+          </div>
+          <div className="footer">
+            <p>Media Make Change 2019</p>
+          </div>
         </header>
       </div>
     );
