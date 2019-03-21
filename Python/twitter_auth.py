66557470115b0bb969cae930bb4a5da9911ac964
Denny Sajan <dennysajan@gmail.com>
2019-03-21 19:35:15 -0400

    Resetting Project
    restarting project with new requirements listed in the wiki

--- Python/twitter_auth.py
+++
@@ -1,6 +0,0 @@
-# Variables for the twitter API
-Access_Token = '125947794-njkGQgfJQS6oJQIT5bCWldcQcclS7OfmYHL3nTOB'
-Access_Token_Secret = 'EZeHLwUb0MPjNdp17ktl27QYsemQvd5BIs0gSj5CXBBLF'
-
-Customer_Token = 'nXcf39pqLARhJhRlRIpzvbheo'
-Customer_Token_Secret = '7quiK1pdbneL3Ol3Jk1htT9xHt8NDEKqQfxCM8RTABaD2oqPFS'
