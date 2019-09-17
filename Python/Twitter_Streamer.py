ca3c5030ff4935ff81da0752d95e76d270b1b87c
Denny Sajan <dennysajan@gmail.com>
2019-09-17 10:44:27 -0400

    updated

--- Python/Twitter_Streamer.py
+++ Python/Twitter_Streamer.py
@@ -1,10 +1,13 @@
 from tweepy import OAuthHandler
 from tweepy import StreamListener
 from tweepy import Stream
-
 import json
-import pymongo
+import os
 
+CA = os.environ['CA']
+CAS = os.environ['CAS']
+AT = os.environ['AT']
+ATS = os.environ['ATS']
 """
 For the streamer we will be using Tweepy which is a twitter API wrapper that makes it easier for us to access information on twitter.
 Tweepy can be installed by using the command 'pip install tweepy' with the terminal on Pycharm. 
@@ -13,10 +16,8 @@ This code will work with Python 3.7.1
 
 class StdOutListener(StreamListener):
     def on_data(self, data):
-        json_data = json.loads(data)
-        print("===================================================")
         print(data)
-        print("===================================================")
+        print()
         return True
 
     def on_error(self, status_code):
@@ -24,8 +25,8 @@ class StdOutListener(StreamListener):
 
 if __name__ == "__main__":
     listener = StdOutListener()
-    auth = OAuthHandler(Consumer_API, Consumer_API_Secret)
-    auth.set_access_token(Access_Token, Access_Token_Secret)
+    auth = OAuthHandler(CA, CAS)
+    auth.set_access_token(AT, ATS)
 
     stream = Stream(auth, listener)
-    stream.filter(track=['trump'])
+    stream.filter()
