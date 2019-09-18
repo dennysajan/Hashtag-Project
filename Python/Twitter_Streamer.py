70525d758986da484121f6cf68f0c46d277a9d53
Denny Sajan <dennysajan@gmail.com>
2019-09-18 16:41:39 -0400

    script can insert data into mongoDB

--- Python/Twitter_Streamer.py
+++ Python/Twitter_Streamer.py
@@ -3,11 +3,18 @@ from tweepy import StreamListener
 from tweepy import Stream
 import os
 import json
+import pymongo
+from ast import literal_eval
 
 CA = os.environ['CA']
 CAS = os.environ['CAS']
 AT = os.environ['AT']
 ATS = os.environ['ATS']
+conn = pymongo.MongoClient('localhost', 27017)
+db = conn['twitter']
+coll = db['tweets_MTA']
+print(coll)
+
 """
 For the streamer we will be using Tweepy which is a twitter API wrapper that makes it easier for us to access information on twitter.
 Tweepy can be installed by using the command 'pip install tweepy' with the terminal on Pycharm. 
@@ -16,8 +23,8 @@ This code will work with Python 3.7.1
 
 class StdOutListener(StreamListener):
     def on_data(self, data):
-        json_data = json.loads(data)
-        print(json_data['id'])
+        json_data_json = json.loads(data)
+        coll.insert_one(json_data_json)
         return True
 
     def on_error(self, status_code):
@@ -29,4 +36,4 @@ if __name__ == "__main__":
     auth.set_access_token(AT, ATS)
 
     stream = Stream(auth, listener)
-    stream.filter(track='trump')
+    stream.filter(track='#MoreThanAnAthlete')
